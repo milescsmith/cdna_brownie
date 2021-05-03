64fa16ef8a4174c6870bb6a02968e7bd3f403fe6
Miles Smith <miles-smith@omrf.org>
2021-05-03 10:13:36 -0500

    first

---
+++ src/cdna_brownie/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Utilities for processing PacBio long-read RNAseq data"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
